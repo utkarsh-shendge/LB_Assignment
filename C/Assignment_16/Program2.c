10554b818eb78904040f1034e8078af6f6e0344b
utkarsh-shendge <utkarsh.g.shendge@gmail.com>
2025-06-11 15:24:02 +0530

    Add files via upload

---
+++ C/Assignment_16/Program2.c
@@ -0,0 +1,44 @@
+/*
+Problem Statement   :   Accept number of rows an number of columns from user and display pattern.
+Input               :   iRow = 4    iCol = 4
+Ouput               :   *   *   *   *
+                        *   *   *
+                        *   *
+                        * 
+                        
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+   int i = 0, j = 0;
+
+   for(i = 1; i <= iRow; i++)
+   {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(((j - i) <= 3) && ((j - i) >= 0)) 
+            {
+                printf("*\t");
+            }
+        }
+                
+        printf("\n");
+   }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
